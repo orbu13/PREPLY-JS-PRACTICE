6e2c022f8ef17722355b7c2d27d0480dc1dfcadf
orbu13 <orelbutbul13@gmail.com>
2024-03-12 15:52:27 -0400

    learning

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+console.log(5%2);
+console.log(2%5);
+console.log(2%2);
+
+let arr =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for (let i = 0; i <= arr.length; i ++){
+    if(arr[i] % 2 == 0 ) console.log(arr[i]);
+}
